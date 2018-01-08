7a2827ae5044a7fc83b4534f1ed7b421310930ab
Th0re <stephane.perrez@gmail.com>
2018-01-08 01:09:10 +0100

    Description ressamblanceCourbes

--- ressamblanceCourbes.m
+++ ressamblanceCourbes.m
@@ -1,6 +1,9 @@
 function [ maxCorrelation ] = ressamblanceCourbes( courbe1, courbe2 )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%ressamblanceCourbes Calcule le coefficient de correlation de deux courbes
+%en essayant de les decaler
+%   coubre1, courbe2 : courbes a comparer
+%   maxCorrelation : coefficient de correlation le plus haut avec les
+%   differents decalages
 
 [nbValeurs, ~] = size(courbe1);
 decalageMax = floor(nbValeurs*0.05);
@@ -9,9 +12,6 @@ maxCorrelation = 0;
 for i=-decalageMax:decalageMax
     ddc =  corrcoef(courbe1(decalageMax+1:nbValeurs-decalageMax),...
         courbe2(decalageMax+i+1:nbValeurs-decalageMax+i));
-%     figure
-%     subplot(1,2,1), plot(courbe1(decalageMax:nbValeurs-decalageMax));
-%     subplot(1,2,2), plot(courbe2(decalageMax-i:nbValeurs-decalageMax-i));
     ddc = ddc(2);
     if(maxCorrelation<ddc)
         maxCorrelation=ddc;
